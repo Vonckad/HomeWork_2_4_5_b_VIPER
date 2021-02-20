00339091c2a8266e48480290e7b0257d3ef2edbb
Vonckad <Vonkad@yandex.ru>
2021-02-20 13:49:37 +0300

    Initial Commit

---
+++ HomeWork_2_4_5_b_VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_2_4_5_b_VIPER
+//
+//  Created by Vlad Ralovich on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
